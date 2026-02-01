0bccd43f38f6de9c6373b526219257a73532d1c7
Pilad <piladt@gmail.com>
2026-02-01 12:56:04 +0500

    Add batch mode to Infinite Fluid Drills (#5304)
    add batch mode to Infinite Fluid Drills (#5304)
    Nether ores asteroid (#5249)

--- src/main/java/gregtech/common/tileentities/machines/multi/MTEOilDrillBase.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTEOilDrillBase.java
@@ -179,13 +179,17 @@ public abstract class MTEOilDrillBase extends MTEDrillerBase implements IMetrics
         return ImmutableList.of(InputBus, OutputHatch, Maintenance, Energy);
     }
 
+    int batchMultiplier = 1;
+
     @Override
     protected void setElectricityStats() {
+        // for a 6.4 second beautiful batch
+        batchMultiplier = batchMode ? 128 : 1;
         this.mEfficiency = getCurrentEfficiency(null);
         this.mEfficiencyIncrease = 10000;
         int tier = Math.max(0, GTUtility.getTier(getMaxInputVoltage()));
         this.mEUt = -7 << (tier << 1); // (1/4) A of current tier when at bottom (7/8) A of current tier while mining
-        this.mMaxProgresstime = calculateMaxProgressTime(tier);
+        this.mMaxProgresstime = calculateMaxProgressTime(tier) * batchMultiplier;
     }
 
     @Override
