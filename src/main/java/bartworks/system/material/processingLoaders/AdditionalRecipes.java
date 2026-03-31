360190c46a6fd58dfd20e7212f70468ae65c7245
Pilad <piladt@gmail.com>
2026-03-31 17:21:29 +0500

    Increase burn values of various acid generator acids (#6244)

--- src/main/java/bartworks/system/material/processingLoaders/AdditionalRecipes.java
+++ src/main/java/bartworks/system/material/processingLoaders/AdditionalRecipes.java
@@ -74,6 +74,7 @@ import bartworks.util.BioPlasmid;
 import gregtech.api.enums.GTValues;
 import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
+import gregtech.api.enums.MaterialsKevlar;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.enums.TierEU;
 import gregtech.api.util.GTModHandler;
@@ -245,17 +246,20 @@ public class AdditionalRecipes {
         }
 
         List<Pair<Materials, Integer>> liquidFuels = Arrays.asList(
-            ImmutablePair.of(Materials.PhosphoricAcid, 36),
-            ImmutablePair.of(Materials.DilutedHydrochloricAcid, 14),
-            ImmutablePair.of(Materials.HypochlorousAcid, 30),
-            ImmutablePair.of(Materials.HydrofluoricAcid, 40),
-            ImmutablePair.of(Materials.HydrochloricAcid, 28),
-            ImmutablePair.of(Materials.NitricAcid, 24),
+            ImmutablePair.of(Materials.PhosphoricAcid, 66),
+            ImmutablePair.of(Materials.DilutedHydrochloricAcid, 26),
+            ImmutablePair.of(Materials.HypochlorousAcid, 56),
+            ImmutablePair.of(Materials.HydrofluoricAcid, 60),
+            ImmutablePair.of(Materials.HydrochloricAcid, 52),
+            ImmutablePair.of(Materials.NitricAcid, 72),
             ImmutablePair.of(Materials.Mercury, 32),
-            ImmutablePair.of(Materials.DilutedSulfuricAcid, 9),
-            ImmutablePair.of(Materials.SulfuricAcid, 18),
-            ImmutablePair.of(Materials.AceticAcid, 11),
-            ImmutablePair.of(WerkstoffLoader.FormicAcid.getBridgeMaterial(), 40));
+            ImmutablePair.of(Materials.DilutedSulfuricAcid, 14),
+            ImmutablePair.of(Materials.SulfuricAcid, 28),
+            ImmutablePair.of(Materials.AceticAcid, 21),
+            ImmutablePair.of(WerkstoffLoader.FormicAcid.getBridgeMaterial(), 40),
+            ImmutablePair.of(WerkstoffLoader.HexafluorosilicicAcid.getBridgeMaterial(), 350),
+            ImmutablePair.of(Materials.PhthalicAcid, 270),
+            ImmutablePair.of(MaterialsKevlar.NaphthenicAcid, 250));
         for (Pair<Materials, Integer> fuel : liquidFuels) {
             GTValues.RA.stdBuilder()
                 .itemInputs(
