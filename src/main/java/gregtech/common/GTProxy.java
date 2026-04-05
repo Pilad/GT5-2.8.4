ddc4988d57969e03c511f235984bf60c6df4021f
Pilad <piladt@gmail.com>
2026-04-06 02:52:21 +0500

    Fix two world server memory leaks caused by pollution and chunk manager (#6296)

--- src/main/java/gregtech/common/GTProxy.java
+++ src/main/java/gregtech/common/GTProxy.java
@@ -1261,6 +1261,8 @@ public class GTProxy implements IFuelHandler {
         GTCapesLoader.clearSelectedCapes();
         PowerGogglesEventHandler.getInstance()
             .onServerStopped(event);
+        GTChunkManager.instance.onServerStopped();
+        dimensionWisePollution.clear();
     }
 
     /**
