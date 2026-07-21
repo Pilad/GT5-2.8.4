adebfc8eb96ffec91bf81d94a3923066fd1a4059
Pilad <piladt@gmail.com>
2026-07-21 12:54:48 +0500

    Fix fluid pipe disconnecting when cover blocks fluid on adjacent block (#7495)

--- src/main/java/gregtech/api/metatileentity/BaseMetaTileEntity.java
+++ src/main/java/gregtech/api/metatileentity/BaseMetaTileEntity.java
@@ -1858,10 +1858,7 @@ public class BaseMetaTileEntity extends CommonBaseMetaTileEntity
 
     @Override
     public FluidTankInfo[] getTankInfo(ForgeDirection side) {
-        if (canAccessData() && (side == ForgeDirection.UNKNOWN
-            || (mMetaTileEntity.isLiquidInput(side) && getCoverAtSide(side).letsFluidIn(null))
-            || (mMetaTileEntity.isLiquidOutput(side) && getCoverAtSide(side).letsFluidOut(null))))
-            return mMetaTileEntity.getTankInfo(side);
+        if (canAccessData()) return mMetaTileEntity.getTankInfo(side);
         return GTValues.emptyFluidTankInfo;
     }
 
